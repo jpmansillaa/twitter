9d15bf1f7fb4630ee7fb2bda1d0208d5c8d886fb
jpmansillaa <juanmansillaalcantara@gmail.com>
2024-12-09 07:50:16 -0300

    armado de estructura y funcionalidades requeridas de la pagina

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    include PgSearch::Model
+
+    pg_search_scope :search_full_text,
+                    against: {
+                        name: 'A',
+                        id: 'B'
+                    }
+end
